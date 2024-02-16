9ee7063dd4cd3d91ac02f922791433da51906fdf
DeathMaroko <asikkoko@gmail.com>
2024-02-16 19:12:56 +0100

    Fix the email standards

--- send_email.py
+++ send_email.py
@@ -1,8 +1,9 @@
 import smtplib, ssl
 import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-
-def send_email(message):
+def send_email(body):
     host = "smtp.gmail.com"
     port = 465
 
@@ -12,8 +13,12 @@ def send_email(message):
     receiver = "majewski.jacek002@gmail.com"
     my_context = ssl.create_default_context()
 
-    message = message.encode("utf-8")
+    message = MIMEMultipart()
+    message["From"] = username
+    message["To"] = receiver
+    message["Subject"] = "Today's news"
+    message.attach(MIMEText(body, "plain"))
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
         server.login(username, authentication)
-        server.sendmail(username, receiver, message)
+        server.sendmail(username, receiver, message.as_string())
