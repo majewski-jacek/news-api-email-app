7a22ae8b3e2c37ff564fe7b18483684bcd278bd0
DeathMaroko <asikkoko@gmail.com>
2024-02-16 13:13:45 +0100

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
+from send_email import send_email
 
 api_key = "e3b4598a90914c73b75d145ac48f4f17"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2024-01-16&sortBy=publishedAt&apiKey=" \
+      "sortBy=publishedAt&apiKey=" \
       "e3b4598a90914c73b75d145ac48f4f17"
 
 request = requests.get(url)
 
 content = request.json()
 
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        raw_title = article["title"].replace("\n", "")
+        raw_description = article["description"].replace("\n", "")
+        body = body + raw_title + "\n" + raw_description + 2*"\n"
+
+send_email(body)
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "app1generator@gmail.com"
+    authentication = os.getenv("AUTHENTICATION")
+
+    receiver = "majewski.jacek002@gmail.com"
+    my_context = ssl.create_default_context()
+
+    message = message.encode("utf-8")
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, authentication)
+        server.sendmail(username, receiver, message)
